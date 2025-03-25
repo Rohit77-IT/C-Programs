c0bc1f6f79379926a3a07674147f155a8bf80e14
Rohit77-IT <dey71393@gmail.com>
2025-03-26 01:35:01 +0530

    int main() {     int n, a = 0, b = 1, next;     printf("Enter the number of terms: ");     scanf("%d", &n);
        printf("Fibonacci Series: ");
        for (int i = 0; i < n; i++) {
            printf("%d ", a);
            next = a + b;
            a = b;
            b = next;
        }
        printf("\n");
    
        return 0;
    }

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,19 +1,18 @@
-# include <stdio.h.>
-int main ()
-{
- int n , a = 0, b=1, next;
- printf("Enter the number of terms: ");
- scanf("%d",&n);
+#include <stdio.h>
 
- printf("Fibonacci series: ");
-    for(int i=0;i<n;i++)
-    {
-    printf("%d", a);
-    next = a+b;
-    a = b;
-    next = a;
-}
-printf("\n");
-
-return 0;
+int main() {
+    int n, a = 0, b = 1, next;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    
+    printf("Fibonacci Series: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    printf("\n");
+    
+    return 0;
 }
